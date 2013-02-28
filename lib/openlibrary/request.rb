483807fe7b754018ab9a117024d2996d5e89cf3e
John Shutt <john.d.shutt@gmail.com>
2013-02-28 02:25:09 -0800

    expand request module

--- lib/openlibrary/request.rb
+++ lib/openlibrary/request.rb
@@ -1,4 +1,35 @@
 require 'rest-client'
-require 'hashie'
+require 'json'
 
-# Add a request module here
+module Openlibrary
+  module Request
+    API_URL     = 'http://www.openlibrary.org'
+
+    protected
+
+    # Perform an API request
+    #
+    # path  - Request path
+    #
+    def request(path)
+      url = "#{API_URL}#{path}"
+
+      resp = RestClient.get(url) do |response, request, result, &block|
+        case response.code
+        when 200
+          response.return!(request, result, &block)
+        when 401
+          raise Openlibrary::Unauthorized
+        when 404
+          raise Openlibrary::NotFound
+        end
+      end
+      parse(resp)
+    end
+
+    def parse(resp)
+      object = JSON.parse(resp.body)
+      object
+    end
+  end
+end
